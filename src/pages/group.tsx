c64106c223478d325a297341ae55be5d1e97974e
shun2218-dev <kohaku1015game@gmail.com>
2022-09-29 16:14:51 +0900

    create group join page

--- src/pages/group.tsx
+++ src/pages/group.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Card from "@/components/card";
 import styles from "@/styles/pages/Group.module.scss";
+import { usePage } from "@/hooks/usePage";
+import { useParams } from "react-router-dom";
 
 const Group = () => {
+  const { toJoin } = usePage();
+  const { uid } = useParams();
   return (
     <>
       <div className={styles.cardContainer}>
-        <Card>Join a already exists group</Card>
+        <Card onClick={() => toJoin(uid!)}>Join a already exists group</Card>
         <Card>Create a new group</Card>
       </div>
     </>
