6218b5362f78cc678c7dafde584c60d1e88a9b12
shun2218-dev <kohaku1015game@gmail.com>
2023-03-11 16:57:48 +0900

    adjust vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
-    },
+      '@': resolve(__dirname, 'src')
+    }
   },
-  base: "./",
-});
+  base: './',
+  server: {
+    cors: {
+      origin: 'https://firebasestorage.googleapis.com'
+    }
+  }
+})
